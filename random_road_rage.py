fa7ad86ba7f4c5facd021d766adc39f5c4451aa9
MC-Riemann <s-dhaber@haw-landshut.de>
2019-10-28 06:43:59 +0100

    add input options add description

--- random_road_rage.py
+++ random_road_rage.py
@@ -1,3 +1,11 @@
+# File to simulate traffic demand using SUMO's randomtrips.py tool.
+
+# Intended to be used for creation of a first model of a city's traffic demand until the output relatively
+# matches the city's actual demand to determine where sensors should be placed. Calibration towards sensors data
+# follows in *filename*
+
+# author: Daniel Ostertag, Daniel Habermayr
+
 import argparse
 import os
 import sys
@@ -25,6 +33,18 @@ def main():
     # Add RRR arguments
     my_parser.add_argument('--prefix', action='store_true', dest='tripprefix', default='',
                            help='prefix for the trip ids')
+    my_parser.add_argument('--truck', action='store', type=float, dest='truck_rate', default=0,
+                           help='percentage of trucks')
+    my_parser.add_argument('--bus', action='store', type=float, dest='bus_rate', default=0,
+                           help='percentage of busses')
+    my_parser.add_argument('--mc', action='store', type=float, dest='mc_rate', default=0,
+                           help='percentage of motorcycles')
+    my_parser.add_argument('--ped', action='store', type=float, dest='ped_rate', default=0,
+                           help='percentage of pedestrians')
+    my_parser.add_arguments('-s', '--seed', action='store', type=int, dest='seed', default=1,
+                            help='seed for the simulation')
+    my_parser.add_arguments('--fringe-factor', action='store', type=float, dest='fringe_factor', default=0.5,
+                            help='seed for the simulation')
 
     args = my_parser.parse_args()
 
